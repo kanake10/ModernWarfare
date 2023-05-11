43b1420432a7e46100a499776808af4e7475c49e
kanake10 <10kanake@gmail.com>
2023-05-11 17:20:17 +0300

    revert graphql changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
-  end
-
   post "/graphql", to: "graphql#execute"
   resources :maps, only: [:index, :show]
   resources :missions, only: [:index, :show, :create, :update]
