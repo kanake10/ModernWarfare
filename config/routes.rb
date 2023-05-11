5bfe8a1ed1edbc1b1a6b09943460c13437fbd872
kanake10 <10kanake@gmail.com>
2023-05-11 17:17:17 +0300

    graphql

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  end
+
   post "/graphql", to: "graphql#execute"
   resources :maps, only: [:index, :show]
   resources :missions, only: [:index, :show, :create, :update]
